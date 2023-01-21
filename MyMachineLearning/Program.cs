aca2d8088ab6fc1aaace315e3438409dce2037d5
Tutto <tuttoandcomeon@outlook.com>
2023-01-22 00:21:56 +0100

    Nueva versión corregida!

--- MyMachineLearning/Program.cs
+++ MyMachineLearning/Program.cs
@@ -1,199 +1,162 @@
-﻿namespace MyMachineLearning;
+﻿using System;
+using System.Linq;
 
-internal class Program
+namespace MyMachineLearning
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        // Crear una nueva instancia de la clase RedNeuronal
-        int numEntradas = 2;
-        int numNeuronasOcultas = 3;
-        int numNeuronasSalida = 1;
-        Func<double, double> funcionActivacion = Sigmoid;
-        var red = new RedNeuronal(numEntradas, numNeuronasOcultas, numNeuronasSalida, funcionActivacion);
-
-        // Entrenar la red con un conjunto de datos de entrenamiento
-        double[][] entradas = new double[4][] { new double[] { 0, 0 }, new double[] { 0, 1 }, new double[] { 1, 0 }, new double[] { 1, 1 } };
-        double[][] salidasEsperadas = new double[4][] { new double[] { 0 }, new double[] { 1 }, new double[] { 1 }, new double[] { 0 } };
-        int numEpocas = 10000;
-        double tasaAprendizaje = 0.1;
-        red.Entrenar(entradas, salidasEsperadas, numEpocas, tasaAprendizaje);
-
-        // Propagar una entrada a través de la red y mostrar la salida
-        double[] entrada = new double[] { 1, 1 };
-        double[] salida = red.PropagarEntrada(entrada);
-        Console.WriteLine("Salida para la entrada [1, 1]: " + salida[0]);
-
-        // espera del usuario para finalizar
-        Console.ReadKey();
-    }
-
-    private static double Sigmoid(double x)
-    {
-        return 1 / (1 + Math.Exp(-x));
-    }
-}
-
-
-/// <summary>
-/// En este ejemplo, la clase Neurona tiene tres atributos: un arreglo de pesos, un bias y una función de activación. El constructor de la clase inicializa los pesos y el bias con valores aleatorios y asigna la función de activación dada. También tiene un método llamado CalcularSalida que toma un arreglo de entradas y devuelve la salida de la neurona. 
-/// En este ejemplo se utiliza una función de activación genérica, pero es importante mencionar que existen diferentes funciones de activación y cada una tiene sus propias características y se utilizan en diferentes contextos.
-/// </summary>
-public class Neurona
-{
-    // Atributos de la neurona
-    public double[] pesos;
-    private double bias;
-    private Func<double, double> funcionActivacion;
-
-    // Constructor
-    public Neurona(int numEntradas, Func<double, double> funcionActivacion)
-    {
-        // Inicializar pesos y bias con valores aleatorios
-        var random = new Random();
-        pesos = new double[numEntradas];
-        for (int i = 0; i < numEntradas; i++)
+        static void Main(string[] args)
         {
-            pesos[i] = random.NextDouble();
-        }
-        bias = random.NextDouble();
+            // Crear una nueva instancia de la clase RedNeuronal
+            int numEntradas = 2;
+            int numNeuronasOcultas = 3;
+            int numNeuronasSalida = 1;
+            Func<double, double> funcionActivacion = Sigmoid;
+            RedNeuronal red = new RedNeuronal(numEntradas, numNeuronasOcultas, numNeuronasSalida, funcionActivacion);
 
-        // Asignar la función de activación
-        this.funcionActivacion = funcionActivacion;
-    }
+            // Entrenar la red con un conjunto de datos de entrenamiento
+            double[][] entradas = new double[4][] { new double[] { 0, 0 }, new double[] { 0, 1 }, new double[] { 1, 0 }, new double[] { 1, 1 } };
+            double[][] salidasEsperadas = new double[4][] { new double[] { 0 }, new double[] { 1 }, new double[] { 1 }, new double[] { 0 } };
+            int numEpocas = 10000;
+            double tasaAprendizaje = 0.1;
+            red.Entrenar(entradas, salidasEsperadas, numEpocas, tasaAprendizaje);
 
-    // Método para calcular la salida de la neurona dada una entrada
-    public double CalcularSalida(double[] entrada)
-    {
-        // Validar que la cantidad de entradas sea correcta
-        if (entrada.Length != pesos.Length)
-        {
-            throw new ArgumentException("La cantidad de entradas debe ser igual a la cantidad de pesos");
+            // Propagar una entrada a través de la red y mostrar la salida
+            double[] entrada = new double[] { 1, 1 };
+            double[] salida = red.PropagarEntrada(entrada);
+            Console.WriteLine("Salida para la entrada [1, 1]: " + salida[0]);
+
+            // espera del usuario para finalizar
+            Console.ReadKey();
         }
 
-        // Calcular la suma ponderada de las entradas
-        double suma = 0;
-        for (int i = 0; i < entrada.Length; i++)
+        private static double Sigmoid(double x)
         {
-            suma += entrada[i] * pesos[i];
-        }
-
-        // Agregar el bias y aplicar la función de activación
-        double salida = funcionActivacion(suma + bias);
-
-        return salida;
-    }
-
-    /*
-    En este ejemplo, se agregó un método llamado ActualizarPesos que toma como argumentos el arreglo de entradas, el error y la tasa de aprendizaje. El método utiliza el algoritmo de retropropagación para calcular el cambio de peso para cada entrada y el bias, y luego actualiza los pesos y el bias.
-    Es importante mencionar que la tasa de aprendizaje es un hiper-parámetro que controla la velocidad de aprendizaje de la red neuronal, una tasa de aprendizaje muy grande puede hacer que la red no converja, mientras que una tasa de aprendizaje muy pequeña puede hacer que la red tardé mucho en converger. Es recomendable experimentar con diferentes valores para encontrar el mejor hiper-parámetro para cada problema en particular.
-     */
-
-    // Método para actualizar los pesos de la neurona
-    public void ActualizarPesos(double[] entrada, double error, double tasaAprendizaje)
-    {
-        // Calcular el cambio de peso para cada entrada
-        for (int i = 0; i < entrada.Length; i++)
-        {
-            double deltaPeso = tasaAprendizaje * error * entrada[i];
-            pesos[i] += deltaPeso;
-        }
-
-        // Actualizar el bias
-        bias += tasaAprendizaje * error;
-    }
-
-}
-
-/// <summary>
-/// En este ejemplo, se creó una clase RedNeuronal que contiene dos arreglos de neuronas, uno para la capa oculta y otro para la capa de salida. El constructor de la clase inicializa estos arreglos con las neuronas necesarias, utilizando el número de entradas, el número de neuronas en cada capa y la función
-/// </summary>
-public class RedNeuronal
-{
-    private Neurona[] capaOculta;
-    private Neurona[] capaSalida;
-
-    // Constructor
-    public RedNeuronal(int numEntradas, int numNeuronasOcultas, int numNeuronasSalida, Func<double, double> funcionActivacion)
-    {
-        // Inicializar capa oculta
-        capaOculta = new Neurona[numNeuronasOcultas];
-        for (int i = 0; i < numNeuronasOcultas; i++)
-        {
-            capaOculta[i] = new Neurona(numEntradas, funcionActivacion);
-        }
-
-        // Inicializar capa de salida
-        capaSalida = new Neurona[numNeuronasSalida];
-        for (int i = 0; i < numNeuronasSalida; i++)
-        {
-            capaSalida[i] = new Neurona(numNeuronasOcultas, funcionActivacion);
+            return 1 / (1 + Math.Exp(-x));
         }
     }
 
-    // Método para propagar la entrada a través de la red
-    public double[] PropagarEntrada(double[] entrada)
+    public class RedNeuronal
     {
-        // Propagar la entrada a través de la capa oculta
-        double[] salidaOculta = new double[capaOculta.Length];
-        for (int i = 0; i < capaOculta.Length; i++)
-        {
-            salidaOculta[i] = capaOculta[i].CalcularSalida(entrada);
-        }
+        public Neurona[] capaOculta { get; set; }
+        public Neurona[] capaSalida { get; set; }
 
-        // Propagar la salida de la capa oculta a través de la capa de salida
-        double[] salida = new double[capaSalida.Length];
-        for (int i = 0; i < capaSalida.Length; i++)
+        public RedNeuronal(int numEntradas, int numNeuronasOcultas, int numNeuronasSalida, Func<double, double> funcionActivacion)
         {
-            salida[i] = capaSalida[i].CalcularSalida(salidaOculta);
-        }
-
-        return salida;
-    }
-
-    // Método para entrenar la red con un conjunto de datos de entrenamiento
-    public void Entrenar(double[][] entradas, double[][] salidasEsperadas, int numEpocas, double tasaAprendizaje)
-    {
-        for (int epoca = 0; epoca < numEpocas; epoca++)
-        {
-            for (int i = 0; i < entradas.Length; i++)
+            // Inicializar capa oculta
+            capaOculta = new Neurona[numNeuronasOcultas];
+            for (int i = 0; i < numNeuronasOcultas; i++)
             {
-                // Propagar la entrada a través de la red
-                double[] salidaOculta = new double[capaOculta.Length];
-                for (int j = 0; j < capaOculta.Length; j++)
-                {
-                    salidaOculta[j] = capaOculta[j].CalcularSalida(entradas[i]);
-                }
-                double[] salida = PropagarEntrada(entradas[i]);
+                capaOculta[i] = new Neurona(numEntradas, funcionActivacion);
+            }
 
-                // Calcular el error de la salida
-                double[] error = new double[salida.Length];
-                for (int j = 0; j < salida.Length; j++)
-                {
-                    error[j] = salidasEsperadas[i][j] - salida[j];
-                }
+            // Inicializar capa de salida
+            capaSalida = new Neurona[numNeuronasSalida];
+            for (int i = 0; i < numNeuronasSalida; i++)
+            {
+                capaSalida[i] = new Neurona(numNeuronasOcultas, funcionActivacion);
+            }
+        }
 
-                // Retropropagar el error a través de la capa de salida
-                for (int j = 0; j < capaSalida.Length; j++)
+        public void Entrenar(double[][] entradas, double[][] salidasEsperadas, int numEpocas, double tasaAprendizaje)
+        {
+            for (int e = 0; e < numEpocas; e++)
+            {
+                for (int i = 0; i < entradas.Length; i++)
                 {
-                    capaSalida[j].ActualizarPesos(salidaOculta, error[j], tasaAprendizaje);
-                }
+                    // Propagar la entrada a través de la red
+                    double[] salidaOculta = PropagarEntrada(entradas[i], capaOculta);
+                    double[] salidaRed = PropagarEntrada(salidaOculta, capaSalida);
 
-                // Calcular el error de la capa oculta
-                double[] errorOculto = new double[salidaOculta.Length];
-                for (int j = 0; j < salidaOculta.Length; j++)
-                {
-                    for (int k = 0; k < error.Length; k++)
+                    // Calcular el error en la capa de salida
+                    double[] erroresSalida = new double[salidaRed.Length];
+                    for (int j = 0; j < erroresSalida.Length; j++)
                     {
-                        errorOculto[j] += error[k] * capaSalida[k].pesos[j];
+                        erroresSalida[j] = salidasEsperadas[i][j] - salidaRed[j];
+                    }
+
+                    // Retropropagar el error a través de la capa de salida
+                    for (int j = 0; j < capaSalida.Length; j++)
+                    {
+                        capaSalida[j].ActualizarPesos(salidaOculta, erroresSalida[j], tasaAprendizaje);
+                    }
+
+                    // Calcular el error en la capa oculta
+                    double[] erroresOcultos = new double[salidaOculta.Length];
+                    for (int j = 0; j < erroresOcultos.Length; j++)
+                    {
+                        double error = 0;
+                        for (int k = 0; k < erroresSalida.Length; k++)
+                        {
+                            error += erroresSalida[k] * capaSalida[k].pesos[j];
+                        }
+                        erroresOcultos[j] = error * DerivadaSigmoid(salidaOculta[j]);
+                    }
+
+                    // Retropropagar el error a través de la capa oculta
+                    for (int j = 0; j < capaOculta.Length; j++)
+                    {
+                        capaOculta[j].ActualizarPesos(entradas[i], erroresOcultos[j], tasaAprendizaje);
                     }
                 }
-
-                // Retropropagar el error a través de la capa oculta
-                for (int j = 0; j < capaOculta.Length; j++)
-                {
-                    capaOculta[j].ActualizarPesos(entradas[i], errorOculto[j], tasaAprendizaje);
-                }
             }
+        }
+
+        public double[] PropagarEntrada(double[] entrada)
+        {
+            double[] salidaOculta = PropagarEntrada(entrada, capaOculta);
+            double[] salidaRed = PropagarEntrada(salidaOculta, capaSalida);
+            return salidaRed;
+        }
+
+        private double[] PropagarEntrada(double[] entrada, Neurona[] capa)
+        {
+            double[] salida = new double[capa.Length];
+            for (int i = 0; i < capa.Length; i++)
+            {
+                salida[i] = capa[i].CalcularSalida(entrada);
+            }
+            return salida;
+        }
+
+        private double DerivadaSigmoid(double x)
+        {
+            return x * (1 - x);
+        }
+    }
+
+    public class Neurona
+    {
+        public double[] pesos;
+        private double bias;
+        private Func<double, double> funcionActivacion;
+
+        public Neurona(int numEntradas, Func<double, double> funcionActivacion)
+        {
+            var rand = new Random();
+            pesos = Enumerable.Range(0, numEntradas).Select(_ => rand.NextDouble()).ToArray();
+            bias = rand.NextDouble();
+            this.funcionActivacion = funcionActivacion;
+        }
+
+        public double CalcularSalida(double[] entrada)
+        {
+            if (entrada.Length != pesos.Length)
+            {
+                throw new ArgumentException("La cantidad de entradas debe ser igual a la cantidad de pesos");
+            }
+
+            double sumaPonderada = entrada.Zip(pesos, (entrada, peso) => entrada * peso).Sum() + bias;
+            return funcionActivacion(sumaPonderada);
+        }
+
+        public void ActualizarPesos(double[] entrada, double error, double tasaAprendizaje)
+        {
+            for (int i = 0; i < pesos.Length; i++)
+            {
+                pesos[i] += tasaAprendizaje * error * entrada[i];
+            }
+            bias += tasaAprendizaje * error;
         }
     }
 }
---
+++ MyMachineLearning/README.md
@@ -0,0 +1,15 @@
+﻿# Red neuronal feedforward en C#
+
+Este código proporciona un ejemplo básico de cómo crear una red neuronal feedforward en C#. La red neuronal está compuesta por dos capas: una capa oculta y una capa de salida. Cada capa está compuesta por un conjunto de neuronas, cada una con sus propios pesos y bias. La red neuronal es entrenada mediante el algoritmo de retropropagación del error y se utiliza una función de activación sigmoid.
+
+## Uso
+Para usar este código, debes seguir estos pasos:
+
+1. Crear una nueva instancia de la clase `RedNeuronal` especificando el número de entradas, el número de neuronas en la capa oculta y en la capa de salida, y la función de activación a utilizar.
+2. Entrenar la red neuronal con un conjunto de datos de entrenamiento específico utilizando el método `Entrenar` de la clase `RedNeuronal`.
+3. Utilizar el método `PropagarEntrada` para propagar una entrada a través de la red y obtener la salida.
+
+## Clases
+- `RedNeuronal`: esta clase representa la red neuronal en sí. Contiene los métodos para entrenar y propagar entradas a través de la red.
+- `Neurona`: esta clase representa una neurona individual en la red. Contiene los pesos y bias de la neurona y un método para calcular la salida de la neurona dada una entrada.
+
