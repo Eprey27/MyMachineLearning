bcb0dc0edbe6f655426bb5e6da0d00cb67dd8946
Tutto <tuttoandcomeon@outlook.com>
2023-01-22 03:02:32 +0100

    version 1.0 completa y funcional

--- MyMachineLearning/Program.cs
+++ MyMachineLearning/Program.cs
@@ -3,8 +3,16 @@ using System.Linq;
 
 namespace MyMachineLearning
 {
+    /// <summary>
+    /// The main program class, containing the entry point of the application.
+    /// </summary>
     internal class Program
     {
+
+        /// <summary>
+        /// Punto de entrada de la aplicación
+        /// </summary>
+        /// <param name="args">Argumentos de línea de comando</param>
         static void Main(string[] args)
         {
             // Crear una nueva instancia de la clase RedNeuronal
@@ -17,12 +25,12 @@ namespace MyMachineLearning
             // Entrenar la red con un conjunto de datos de entrenamiento
             double[][] entradas = new double[4][] { new double[] { 0, 0 }, new double[] { 0, 1 }, new double[] { 1, 0 }, new double[] { 1, 1 } };
             double[][] salidasEsperadas = new double[4][] { new double[] { 0 }, new double[] { 1 }, new double[] { 1 }, new double[] { 0 } };
-            int numEpocas = 10000;
+            int numEpocas = 30000;
             double tasaAprendizaje = 0.1;
             red.Entrenar(entradas, salidasEsperadas, numEpocas, tasaAprendizaje);
 
             // Propagar una entrada a través de la red y mostrar la salida
-            double[] entrada = new double[] { 1, 1 };
+            double[] entrada = new double[] { 0, 1 };
             double[] salida = red.PropagarEntrada(entrada);
             Console.WriteLine("Salida para la entrada [1, 1]: " + salida[0]);
 
@@ -30,6 +38,11 @@ namespace MyMachineLearning
             Console.ReadKey();
         }
 
+        /// <summary>
+        /// La función de activación utilizada por la red neuronal
+        /// </summary>
+        /// <param name="x">Entrada para la función</param>
+        /// <returns>La salida de la función</returns>
         private static double Sigmoid(double x)
         {
             return 1 / (1 + Math.Exp(-x));
